290e29d6609efab4b530c8b6ea7582e0ada8516c
unknown <hibit_at@hotmail.com>
2021-06-18 05:55:54 +0900

    third

--- add.py
+++ add.py
@@ -10,11 +10,9 @@ sys.path.append('takenyan')
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'takenyan.settings')
 django.setup()
 
+if os.path.exists('local.py'):
+    from local import AT, ATS, CK, CS
 
-CK = "3AO7kDWykGtn5mESuxY6k2YRd"
-CS = "VRHJ0bQwzR8BFXXnQSiMxJrweup6UM0lwtajmlTqzH6MIuzGqw"
-AT = "1153375644266328065-qswAlOHMRCWXNlj6ATvDFAnMpBcAMZ"
-ATS = "3VD0WbOJmmU1aJdlyhgpHEPgYMtnJ3snq1aJwfOJ1VXpb"
 twitter = OAuth1Session(CK, CS, AT, ATS)  # 認証処理
 
 url = 'https://api.twitter.com/1.1/search/tweets.json'
@@ -30,6 +28,7 @@ if req.status_code == 200:
     res = json.loads(req.text)
     for line in res['statuses']:
         if 'ねねばー' in line['text']:
+            print(line)
             dt = datetime.strptime(
                 line['created_at'], '%a %b %d %H:%M:%S %z %Y')
             dt = dt + timedelta(hours=9)
