41e2743e11931506b797cbb4703f8bf0b507eb8f
unknown <hibit_at@hotmail.com>
2021-06-21 20:17:48 +0900

    manu

--- app/urls.py
+++ app/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('img_plot',views.img_plot,name='img_plot'),
     path('regist', views.regist, name='regist'),
+    path('confirm', views.confirm, name='confirm'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
 import io
 import json
 import os
+import re
 from datetime import datetime, timedelta
 
 import matplotlib
@@ -27,12 +28,14 @@ matplotlib.use('Agg')
 start = ''
 end = ''
 
+
 def plt2png():
     buf = io.BytesIO()
     plt.savefig(buf, format='png', dpi=200)
     s = buf.getvalue()
     buf.close()
     return s
+
 
 def img_plot(response):
     plt.cla()
@@ -57,12 +60,12 @@ def img_plot(response):
     plt.ylim(18, 30)
     start = response.GET.get('start')
     end = response.GET.get('end')
-    sp = list(map(int,start.split('-')))
-    start_time = datetime(sp[0],sp[1],sp[2])
-    ep = list(map(int,end.split('-')))
-    end_time = datetime(ep[0],ep[1],ep[2])
-    print(sp,ep)
-    plt.xlim(start_time,end_time)
+    sp = list(map(int, start.split('-')))
+    start_time = datetime(sp[0], sp[1], sp[2])
+    ep = list(map(int, end.split('-')))
+    end_time = datetime(ep[0], ep[1], ep[2])
+    print(sp, ep)
+    plt.xlim(start_time, end_time)
     ax.xaxis.set_major_formatter(mdates.DateFormatter("%m-%d"))
 
     png = plt2png()
@@ -81,23 +84,23 @@ def index(request):  # 追加
     e_day = recent[0].dt.day
     if recent[0].dt.hour < 12:
         e_day -= 1
-    end = '{}-{}-{}'.format(e_year,e_month,e_day)
-    start = '{}-{}-{}'.format(e_year,e_month,e_day-7)
+    end = '{}-{}-{}'.format(e_year, e_month, e_day)
+    start = '{}-{}-{}'.format(e_year, e_month, e_day-7)
     if 'start' in request.POST and request.POST['start'] != '':
         start = request.POST['start']
     if 'end' in request.POST and request.POST['end'] != '':
         end = request.POST['end']
-    print(start,end)
-    print(type(start),type(end))
-    params = {'data': data, 'recent': recent, 'start' : start, 'end' : end}
+    print(start, end)
+    print(type(start), type(end))
+    params = {'data': data, 'recent': recent, 'start': start, 'end': end}
     return render(request, 'index.html', params)
 
-def regist(request,url='',author='',idnum='',res='error'):
-    if 'url' in request.POST:
+
+def regist(request, url='', author='', idnum='', res='error'):
+    if 'url' in request.POST and request.POST['url'] != '':
         url = request.POST['url']
-        import re
         pattern = r'https://twitter.com/(.*?)/status/(.*)'
-        result = re.findall(pattern,url)
+        result = re.findall(pattern, url)
         print(result)
         author = result[0][0]
         idnum = result[0][1]
@@ -108,5 +111,27 @@ def regist(request,url='',author='',idnum='',res='error'):
             res = json.loads(req.text)
         else:
             res = 'error'
-    params = {'target' : url, 'author' : author, 'idnum' : idnum, 'res' : res}
+    params = {'target': url, 'author': author, 'idnum': idnum, 'res': res}
     return render(request, 'regist.html', params)
+
+
+def confirm(request,addid=''):
+    if 'addid' in request.POST:
+        addid = request.POST['addid']
+    api_url = 'https://api.twitter.com/1.1/statuses/show.json'
+    params = {'id': addid}
+    print(addid)
+    req = twitter.get(api_url, params=params)
+    if req.status_code == 200:
+        res = json.loads(req.text)
+        dt = datetime.strptime(
+            res['created_at'], '%a %b %d %H:%M:%S %z %Y')
+        dt = dt + timedelta(hours=9)
+        usr = '{}(@{})'.format(res['user']['name'],res['user']['screen_name'])
+        Tweet.objects.update_or_create(
+            tw_id=res['id_str'], dt=dt, usr=usr, txt=res['text'])
+        print(addid,"success")
+    else:
+        res = 'error'
+    params = {'addid' : addid}
+    return render(request, 'confirm.html', params)
